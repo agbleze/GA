389469022b87e63f668c7781172d55868813ae69
Agbleze <62272559+agbleze@users.noreply.github.com>
2021-08-23 21:51:03 +0200

    Update init.R

--- init.R
+++ init.R
@@ -1,8 +1,12 @@
 # init.R
+#
 # Example R code to install packages if not already installed
 #
 
-my_packages = c("shiny")
+my_packages = c("shiny", "shinydashboard", "shinycssloaders", "shinyanimate", "shinydashboardPlus",
+                "shinyEffects", "shinybusy", "shinyBS", "magrittr", "waiter", "fresh", "readr", "forecast",
+                "tidyverse", "ggplot2", "fpp2", "lubridate", "GGally", "dplyr", "magrittr", "labelled", "gtsummary",
+                "bfast", "ggstatsplot", "googleVis", "formattable", "fontawesome")
 
 install_if_missing = function(p) {
   if (p %in% rownames(installed.packages()) == FALSE) {
